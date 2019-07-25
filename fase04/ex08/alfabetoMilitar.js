e22f522d32b260e2b98e3c7f15b15f1d15175793
Nayoliveira2322 <44588999+Nayoliveira2322@users.noreply.github.com>
2019-07-25 20:24:17 -0300

    alfabetoMilitar

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,20 @@
+function alfabetoMilitar(l1) {
+    var l = l1.toLowerCase() 
+    if (l == "a") {
+        return "alpha"
+    }
+    if (l == "e") { 
+        return "echo"
+    }
+    if (l == "i") {
+        return "india"
+    }
+    if (l == "o") {
+        return "oscar" 
+    }
+    if (l == "u") {
+        return "uniform"
+    }else{
+        return "false"
+    }
+}
