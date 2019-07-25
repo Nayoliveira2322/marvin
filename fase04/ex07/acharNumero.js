a2f49ecee1ea84cb028cda6b57b331fec722e1f3
Nayoliveira2322 <44588999+Nayoliveira2322@users.noreply.github.com>
2019-07-25 19:56:19 -0300

    acharNumero

---
+++ fase04/ex07/acharNumero.js
@@ -0,0 +1,14 @@
+function acharNumero (n1) {
+    if (n1 < 5) {
+        return "a";
+    }
+    if (n1 >=5 & n1 <=10); {
+    return "b";
+    }
+    if (n1 >10 & n1 < 100); { 
+    return "c"; 
+    }
+    if (n1 > 100); {
+    return "d";
+}
+}
