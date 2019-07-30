dc94e60d74a93c52993ab69619980bb7dfb4b182
Nayoliveira2322 <44588999+Nayoliveira2322@users.noreply.github.com>
2019-07-30 19:29:16 -0300

    nova

--- fase04/ex01/nove.js
+++ fase04/ex01/nove.js
@@ -2,8 +2,8 @@ function nove (num) {
     var n1 = num.toString() ;
     var n2 = n1.substr(-1);
     if (n2 === "9") {
-        return "true" ;
+        return true ;
     }else{ 
-        return "false";
+        return false;
     }
 }
